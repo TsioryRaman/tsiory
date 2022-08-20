256dd2d4a644f4dbfd05468306a02581f8df087d
tsioryRaman <rtsiory4@gmail.com>
2022-08-20 18:01:41 +0300

    feat:add reveal

--- components/BlocMain.tsx
+++ components/BlocMain.tsx
@@ -2,6 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 import React from "react"
 import { Award } from "react-feather";
 import { ParcourInformation } from "./ParcoursInformation";
+import Fade from "react-reveal"
 
 export const BlockMain: React.FC = () => {
     return (
@@ -13,17 +14,25 @@ export const BlockMain: React.FC = () => {
                 <Text as='h2' fontSize='3xl' fontWeight='700'>
                     Mes parcours
                 </Text>
-                <ParcourInformation year='2022' content={[{ header: "MasterI à l'ISPM", description: `Etude supérieur en 4ème année à l'ISPM`, iconFinal: <span>&#129333;</span> }]} ></ParcourInformation>
+                <Fade>
+                    <ParcourInformation year='2022' content={[{ header: "MasterI à l'ISPM", description: `Etude supérieur en 4ème année à l'ISPM`, iconFinal: <span>&#129333;</span> }]} ></ParcourInformation>
+                </Fade>
+                <Fade>
+                    <ParcourInformation year='2021'
+                        content={[{ header: "MasterI à l'ISPM", description: `Etude supérieur en 4ème année à l'ISPM`, iconFinal: <span>&#129333;</span> },
+                        { header: "Soutenance 3ème année en Informatique et Telecommuniaction", description: `Présentation de ma soutenance "Transfert de messagerie et de fichier en temps réel"`, iconFinal: <Award display="inline" /> },
+                        { header: "Stage au sein de la société Esokia Madagascar", description: "Stage professionelle au sein de la société Esokia en tant que stagiaire Back-end (Symfony/Magento2)" }]} />
+                    <ParcourInformation year='2020' content={[{ header: "Fin d'etude universitaire en 3ème année", description: "Fin du parcous en L3 (licence) pour le parcours Informatique et Télécommunication à l'ISPM" }]} />
+                </Fade>
+                <Fade>
+                    <ParcourInformation year='2017' content={[{ header: "Entrer à l'université", description: "Debut de formation en Informatique et Telecommunication.Sans oublier l'apprentissage autodidacte.", iconFinal: <span>&#128521;</span> }]} />
 
-                <ParcourInformation year='2021'
-                    content={[{ header: "MasterI à l'ISPM", description: `Etude supérieur en 4ème année à l'ISPM`, iconFinal: <span>&#129333;</span> },
-                    { header: "Soutenance 3ème année en Informatique et Telecommuniaction", description: `Présentation de ma soutenance "Transfert de messagerie et de fichier en temps réel"`, iconFinal: <Award display="inline" /> },
-                    { header: "Stage au sein de la société Esokia Madagascar", description: "Stage professionelle au sein de la société Esokia en tant que stagiaire Back-end (Symfony/Magento2)" }]} />
-                <ParcourInformation year='2020' content={[{ header: "Fin d'etude universitaire en 3ème année", description: "Fin du parcous en L3 (licence) pour le parcours Informatique et Télécommunication à l'ISPM" }]} />
+                </Fade>
+                <Fade>
+                    <ParcourInformation year='2016' content={[{ header: "Entrer à l'université(CNTEMAD)", description: "Debut de formation en Telecommunication en licence 1 pour avoir de meilleurs base en Mathematique.", iconFinal: <span>&#128521;</span> },
+                    { header: 'Obtention du baccalaureat', description: "Bachelier en serie scientifique" }]} />
+                </Fade>
 
-                <ParcourInformation year='2017' content={[{ header: "Entrer à l'université", description: "Debut de formation en Informatique et Telecommunication.Sans oublier l'apprentissage autodidacte.", iconFinal: <span>&#128521;</span> }]} />
-                <ParcourInformation year='2016' content={[{ header: "Entrer à l'université(CNTEMAD)", description: "Debut de formation en Telecommunication en licence 1 pour avoir de meilleurs base en Mathematique.", iconFinal: <span>&#128521;</span> },
-                { header: 'Obtention du baccalaureat', description: "Bachelier en serie scientifique" }]} />
             </Box>
         </Box >
     );
