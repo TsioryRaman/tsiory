92b6c1752fa0b44c0255e66fac90a69a043bf568
tsioryRaman <rtsiory4@gmail.com>
2022-09-11 11:36:06 +0300

    build:fix stack in divertissement

--- components/Divertissement.tsx
+++ components/Divertissement.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react"
 
 type Feature = {
