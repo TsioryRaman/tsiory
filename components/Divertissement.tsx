2934ab3f55f1235da408ab75b5c96665f0671114
tsioryRaman <rtsiory4@gmail.com>
2022-09-11 11:34:19 +0300

    fix:width card in divertissement.tsx

--- components/Divertissement.tsx
+++ components/Divertissement.tsx
@@ -1,19 +1,17 @@
-import { Box, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import React from "react"
 
 type Feature = {
     title: String,
     desc: String,
     img?: string,
-    heightImg?: string,
-    widthImg?: string
 }
 
-const Feature: React.FC<Feature> = ({ title, desc, img, heightImg, widthImg, ...rest }) => {
+const Feature: React.FC<Feature> = ({ title, desc, img, ...rest }) => {
     return (
         <Box p={5} shadow='md' borderWidth='1px' overflow={"hidden"} {...rest}>
             {img && <Box overflow="hidden">
-                <Image src={"/img/" + img} cursor="pointer" alt={img} height={heightImg} width={widthImg} _hover={{ transform: "scale(1.2)" }} transitionDuration={".8s"} />
+                <Image src={"/img/" + img} cursor="pointer" alt={img} height={["400px"]} width={["100%","100%","100%","100%"]} _hover={{ transform: "scale(1.2)" }} transitionDuration={".8s"} />
             </Box>}
             <Heading mt='4' fontSize='xl'>{title}</Heading>
             <Text mt={4}>{desc}</Text>
@@ -23,26 +21,22 @@ const Feature: React.FC<Feature> = ({ title, desc, img, heightImg, widthImg, ...
 
 const Divertissement: React.FC = () => {
     return (
-        <Box gap={8} display="flex" flexShrink={'unset'} flexDirection={["column", "column", "column", "row"]} >
-            <Stack direction='row' height={"fit-content"} mx={["auto", "auto", "auto", 0]}>
+        <Flex gap={8} flexDirection={["column", "column", "column", "row"]} >
+            <Box width={["100%","100%","100%","30%"]} mx={["auto", "auto", "auto", 0]}>
                 <Feature
                     title='Bon plan 😂😂'
                     desc='Regarder des films et des dessins animes😎😎'
                     img='the_amazing_spider_man_le_destin_d_un_heros.jpg'
-                    heightImg="auto"
-                    widthImg="100%"
                 />
-            </Stack>
-            <Stack>
+            </Box>
+            <Box width={["100%","100%","100%","70%"]}>
                 <Feature
-                    heightImg="auto"
-                    widthImg="100%"
                     title='Jouer au jeu, surtout les jeux en ligne 🏹🏆'
                     desc='Cela nous offre divers facon de trouver des coequipiers a qui on peut former une equipe comme dans la vie professionelle'
                     img='pubg.jpg'
                 />
-            </Stack>
-        </Box>
+            </Box>
+        </Flex>
     );
 }
 export default Divertissement
