9884058299d1dc16136e3c27a4cace1bb8c5f82a
tsioryRaman <rtsiory4@gmail.com>
2022-09-18 18:28:08 +0300

    fix:card article mobile

--- components/Article.tsx
+++ components/Article.tsx
@@ -38,13 +38,13 @@ export const Article: React.FC<ArticleType> = ({ title, stack, description, id }
                     {description}
                 </Text>
     </Box>*/}
-            <Flex cursor="pointer" boxShadow={"lg"} my="8" borderRadius="lg" overflow={"hidden"} direction={["column", "column", "row", "row"]} w="full">
+            <Flex cursor="pointer" boxShadow={"lg"} my="8" borderRadius={["xl", "xl", "lg", "lg"]} overflow={"hidden"} direction={["column", "column", "row", "row"]} w="full">
                 <Flex color="gray.300" direction={"column"} p="8" pl={["8", "8", "8", "12"]} w={["100%", "100%", "35%", "35%"]} bg="blue.900">
-                    <Text as="h4">COURSE</Text>
+                    <Text textTransform={"uppercase"} as="h4">COURS</Text>
                     <Heading mt="4" mb="4" fontWeight={"bold"} fontSize={"1.5em"} as="h1">{title}</Heading>
                     <Text display={"flex"} flexDirection="row" gap="2">Voir le chapitre<ChevronRight /></Text>
                 </Flex>
-                <Flex bg="gray.100" color="gray.700" direction={"column"} p="8" w={["100%", "100%", "65%", "65%"]}>
+                <Flex bg="gray.100" mt={["-18", "-18", "0", "0"]} borderTopRadius={["2xl", "2xl", "0", "0"]} color="gray.700" direction={"column"} p="8" pt={["16", "16", "8", "8"]} w={["100%", "100%", "65%", "65%"]}>
                     <Flex alignItems={"center"} justifyContent={"space-between"} direction={"row"}>
                         <Text fontWeight={"medium"}>ARTICLE {id}</Text>
                         <Progress bg="gray.300" width={"150px"} borderRadius="lg" value={80} />
