c5cf44b2de21cac06f551366dc004894b5ff9ec2
tsioryRaman <rtsiory4@gmail.com>
2022-10-09 08:39:53 +0300

    fix: card divertissement

--- components/Divertissement.tsx
+++ components/Divertissement.tsx
@@ -1,6 +1,6 @@
-import { Avatar, Box, Divider, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react"
-import { Coffee, Video } from "react-feather";
+import { Film, Monitor, Music } from "react-feather";
 
 type Feature = {
     title: String,
@@ -8,21 +8,19 @@ type Feature = {
     img?: string,
 }
 
-const Feature: React.FC<Feature> = ({ title, desc, img, ...rest }) => {
+const Feature: React.FC<Feature> = ({ title, desc, img,children, ...rest }) => {
     return (
-        <Box mt={8} height="100%" borderRadius={8} shadow='md' borderWidth='1px' overflow={"hidden"} {...rest}>
-            {img && <Box position="relative" borderBottomLeftRadius={48} overflow="hidden">
-                
-                <Image src={"/img/" + img} cursor="pointer" alt={img} height={["400px"]} width="100%" _hover={{ transform: "scale(1.2)" }} transitionDuration={".8s"} />
-            </Box>}
-            <Box p={4} position="relative">
-                <Box position="absolute" top="-8" right="6" >
-                    <Avatar bg="green.400" icon={<Video size="20px"  color="white" />}/>
-                </Box>
-                <Heading mb="4" display="flex" justifyContent="space-between" color='#53c9c6' mt='4' fontSize='xl'>{title}<Coffee color="#ED64A6"/></Heading>
+        <Box mt={8} height="100%" shadow='md' borderWidth='1px' {...rest}>
+            <Flex flexDirection="column" height="100%" p={4}  position="relative">
+                <Heading mb="4" display="flex" justifyContent="space-between" color='#53c9c6' mt='4' fontSize='xl'>{title}{children}</Heading>
                 <Divider />
                 <Text fontWeight={"medium"} mt={4}>{desc}</Text>
-            </Box>
+
+                {img && <Box mt="auto" position="relative" marginLeft={["-6","-8","-16","-16"]}>
+                
+                <Image transform="scaleX(-1)" src={"/img/" + img} cursor="pointer" zIndex="2" alt={img} height={["400px"]} width="100%" _hover={{ transform: "scale(-1.2,1.2)",zIndex:"1000" }} transitionDuration={".8s"} />
+            </Box>}
+            </Flex>
         </Box>
     )
 }
@@ -30,20 +28,28 @@ const Feature: React.FC<Feature> = ({ title, desc, img, ...rest }) => {
 const Divertissement: React.FC = () => {
     return (
         <Flex gap={8} flexDirection={["column", "column", "row", "row"]} >
-            <Box width={["100%","100%","50%","30%"]}>
+            <Box width={["100%","100%","50%","50%"]}>
                 <Feature
                     title='Bon plan 😂😂'
                     desc='Regarder des films et des dessins animes😎😎'
-                    img='the_amazing_spider_man_le_destin_d_un_heros.jpg'
-                />
+                    img='spiderman.png'
+                ><Film /></Feature>
             </Box>
-            <Box width={["100%","100%","50%","70%"]}>
+            <Box width={["100%","100%","50%","50%"]}>
+                <Feature
+                    title='Ecouter de la musique'
+                    desc='Cela nous offre divers facon de trouver des coequipiers a qui on peut former une equipe comme dans la vie professionelle'
+                    img='musique.png'
+                ><Music color="purple"/></Feature>
+            </Box>
+            <Box width={["100%","100%","50%","50%"]}>
                 <Feature
                     title='Jouer au jeu video 🏹🏆'
                     desc='Cela nous offre divers facon de trouver des coequipiers a qui on peut former une equipe comme dans la vie professionelle'
-                    img='pubg.jpg'
-                />
+                    img='pubg.png'
+                ><Monitor /></Feature>
             </Box>
+
         </Flex>
     );
 }
