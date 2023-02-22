f2bed639ccacd71315bdf26570bc84e2c3be052e
TsioryRaman <rtsiory4@gmail.com>
2023-02-22 10:17:47 +0300

    fix header nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Flex, Heading, Image, Text, useColorModeValue } from '@chakra-ui/react';
+import { Button, Flex, Heading, Image, Text, useColorModeValue } from '@chakra-ui/react';
 import { TypeAnimation } from 'react-type-animation';
 import { Facebook, GitHub, Linkedin } from 'react-feather';
 import Fade from "react-reveal"
@@ -39,10 +39,25 @@ export const Header: React.FC<HeaderType> = ({ name }) => {
       />
         <span style={{ display: "block" }}>Symfony/React.(js/ts)/Next.(js/ts)<Image src="/img/stack image/next.png" height="40px" width="40px" display="inline" /> | Typescript</span></Text>
 
-      <Flex w='full' direction={'row'} mt='2' gap={'6'}>
-        <a href='https://web.facebook.com/RamanantoaninaTsiory/' target="_blank"><Facebook /></a>
-        <a href='https://www.linkedin.com/in/tsiory-ramanantoanina/' target="_blank"><Linkedin /></a>
-        <a href='https://github.com/TsioryRaman' target="_blank"><GitHub /></a>
+      <Flex w='full' direction={['row']} flexWrap="wrap" mt='16' gap={['2','2','4','6']}>
+
+      <a href='https://web.facebook.com/RamanantoaninaTsiory/' target="_blank">
+        <Button colorScheme={"facebook"}>
+        Facebook<Facebook size="17px"/>
+        </Button>
+      </a>
+
+      <a href='https://www.linkedin.com/in/tsiory-ramanantoanina/' target="_blank">
+        <Button  colorScheme={"linkedin"}>
+          <Text mr="2">Linkedin</Text><Linkedin size="17px"/>
+        </Button>
+      </a> 
+      <a href='https://github.com/TsioryRaman' target="_blank">
+        <Button  variant="outline" colorScheme={"blackAlpha"} color="whiteAlpha.900">
+          <Text mr="2">Github</Text>
+         <GitHub size="17px"/>
+        </Button>
+      </a>
       </Flex>
     </Flex >
 
