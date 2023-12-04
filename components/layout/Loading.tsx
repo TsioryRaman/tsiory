4f3d967e40c176a859c157c5cb55fba3fe665bb4
tsioryRaman <rtsiory4@gmail.com>
2023-12-04 20:48:07 +0300

    fix: loading spinner

--- components/layout/Loading.tsx
+++ components/layout/Loading.tsx
@@ -1,8 +1,11 @@
-import { Flex, Spinner,Text } from "@chakra-ui/react"
+import { Flex, useColorModeValue } from "@chakra-ui/react"
+import { HashLoader } from "react-spinners"
 
 export const Loading = () => {
+
+    const colorLoader = useColorModeValue("#3b82f6","#1e3a8a")
+
     return <Flex as="div" height="100%" minH="100vh" direction="column" justify="center" alignItems="center" w="100%">
-        <Spinner />
-        <Text fontSize="2xl">Chargement...</Text>
+        <HashLoader color={colorLoader} />
     </Flex>
 }
