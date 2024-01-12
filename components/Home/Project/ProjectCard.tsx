0642e2fe5f92afdd70bb3272e56978d3980e975c
TsioryRaman <rtsiory4@gmail.com>
2024-01-12 13:16:53 +0300

    feat: add loading image

--- components/Home/Project/ProjectCard.tsx
+++ components/Home/Project/ProjectCard.tsx
@@ -5,9 +5,10 @@ import {
   useColorModeValue,
   Badge,
   Flex,
+  Image
 } from "@chakra-ui/react";
-import { useEffect } from "react";
 import Fade from "react-reveal"
+import { HashLoader } from "react-spinners";
 
 
 export interface ProjectCardProps {
@@ -32,14 +33,14 @@ export const ProjectCard:React.FC<ProjectCardProps> = ({
           <Fade distance="100px" bottom duration={800}>
 
     <Box  shadow="2xl" position="relative" rounded="2xl" role="group" transitionDuration="300" _hover={{overflow:"visible"}} w={["full","full","700px","700px"]} height="400px">
-        <Box overflow="hidden" h="full" rounded="2xl" >
-            <Box  h="full" 
-                bg={`linear-gradient(to bottom, ${gradient} 0.3), ${gradient} 0.7)),url(${img})`} transitionDuration="300ms" 
-                _groupHover={{transform:"scale(1.1)"}}
-                 roundedBottomRight="0" w="full" bgPosition="center" bgSize="cover" bgRepeat="no-repeat">
+        <Box overflow="hidden" position="relative" h="full" rounded="2xl" >
+            <Box  h="full" position="absolute" height="full" width="full" top="0" left="0"
+                bg={`linear-gradient(to bottom, ${gradient} 0.3), ${gradient} 0.7))`}
+                 roundedBottomRight="0" w="full" zIndex={2}>
+            </Box>
+            <Image zIndex={1} _groupHover={{transform:"scale(1.1)"}} alt={img} transitionDuration="300ms" src={img} bgSize="cover" fallback={<HashLoader />}/>
         </Box>
-        </Box>
-      <Box mx="8" transform="translateY(-50%)" position="relative">
+      <Box mx="8" zIndex={3} transform="translateY(-50%)" position="relative">
         <Box bottom="0"  px="8" py="6" bg={bg} roundedTop="2xl" zIndex={10} >
         <Heading color={text} as="h2" size="lg">
             {title}
